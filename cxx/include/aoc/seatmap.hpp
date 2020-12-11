2f859521cb3f970011ecc6bd5fae55028dbe9384
Stephen Newell <sjnewell@gmail.com>
2020-12-10 22:59:29 -0700

    Solve 11a

---
+++ cxx/include/aoc/seatmap.hpp
@@ -0,0 +1,36 @@
+#ifndef AOC_SEATMAP_HPP
+#define AOC_SEATMAP_HPP 1
+
+#include <istream>
+#include <string>
+
+namespace aoc2020
+{
+    struct SeatNeighbors
+    {
+        std::size_t occupied;
+        std::size_t available;
+    };
+
+    struct SeatMap
+    {
+        SeatNeighbors get_neighbor_info(std::size_t row, std::size_t col) const
+            noexcept;
+
+        friend bool operator==(SeatMap const & lhs,
+                               SeatMap const & rhs) noexcept;
+
+        friend bool operator!=(SeatMap const & lhs,
+                               SeatMap const & rhs) noexcept;
+
+        std::string seats;
+        std::size_t row_count;
+        std::size_t row_length;
+    };
+
+    SeatMap read_seat_map(std::istream & input);
+
+    SeatMap run_cycle(SeatMap const & previous);
+} // namespace aoc2020
+
+#endif
---
+++ cxx/seatmap.cpp
@@ -0,0 +1,134 @@
+#include <aoc/seatmap.hpp>
+
+#include <cassert>
+
+namespace aoc2020
+{
+    SeatMap read_seat_map(std::istream & input)
+    {
+        std::string line;
+        std::getline(input, line);
+
+        SeatMap ret;
+        ret.seats = line;
+        ret.row_length = line.size();
+        ret.row_count = 1;
+        while(std::getline(input, line))
+        {
+            assert(line.size() == ret.row_length);
+            ret.seats += line;
+            ++ret.row_count;
+        }
+        assert(ret.seats.size() == (ret.row_length * ret.row_count));
+        return ret;
+    }
+
+    SeatNeighbors SeatMap::get_neighbor_info(std::size_t row,
+                                             std::size_t col) const noexcept
+    {
+        SeatNeighbors ret{0, 0};
+
+        auto adjust_correct = [&ret](auto seat) {
+            if(seat == '#')
+            {
+                ++ret.occupied;
+            }
+            else
+            {
+                ++ret.available;
+            }
+        };
+
+        auto get_seat = [this](auto row, auto col) {
+            return seats[(row * row_length) + col];
+        };
+
+        if(row != 0)
+        {
+            // we're not at the top
+            if(col != 0)
+            {
+                // we're not at the far left
+                adjust_correct(get_seat(row - 1, col - 1));
+            }
+            adjust_correct(get_seat(row - 1, col));
+            if(col != (row_length - 1))
+            {
+                // we're not at the far left
+                adjust_correct(get_seat(row - 1, col + 1));
+            }
+        }
+        if(col != 0)
+        {
+            // we're not at the far left
+            adjust_correct(get_seat(row, col - 1));
+        }
+        if(col != (row_length - 1))
+        {
+            // we're not at the far left
+            adjust_correct(get_seat(row, col + 1));
+        }
+        if(row != (row_count - 1))
+        {
+            // we're not at the bottom
+            if(col != 0)
+            {
+                // we're not at the far left
+                adjust_correct(get_seat(row + 1, col - 1));
+            }
+            adjust_correct(get_seat(row + 1, col));
+            if(col != (row_length - 1))
+            {
+                // we're not at the far left
+                adjust_correct(get_seat(row + 1, col + 1));
+            }
+        }
+        return ret;
+    }
+
+    SeatMap run_cycle(SeatMap const & previous)
+    {
+        auto ret = previous;
+        for(decltype(ret.row_count) row = 0; row < ret.row_count; ++row)
+        {
+            for(decltype(ret.row_length) col = 0; col < ret.row_length; ++col)
+            {
+                auto & seat = ret.seats[(row * ret.row_length) + col];
+                if(seat != '.')
+                {
+                    auto const neighbors = previous.get_neighbor_info(row, col);
+                    if(seat == 'L')
+                    {
+                        if(neighbors.occupied == 0)
+                        {
+                            seat = '#';
+                        }
+                    }
+                    else
+                    {
+                        assert(seat == '#');
+                        if(neighbors.occupied >= 4)
+                        {
+                            seat = 'L';
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    bool operator==(SeatMap const & lhs, SeatMap const & rhs) noexcept
+    {
+        assert(lhs.row_count == rhs.row_count);
+        assert(lhs.row_length == rhs.row_length);
+        return lhs.seats == rhs.seats;
+    }
+
+    bool operator!=(SeatMap const & lhs, SeatMap const & rhs) noexcept
+    {
+        assert(lhs.row_count == rhs.row_count);
+        assert(lhs.row_length == rhs.row_length);
+        return lhs.seats != rhs.seats;
+    }
+} // namespace aoc2020
